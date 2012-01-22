d7e061028ba96f31bf5b29dbae9e53a71f4d01b3
Stefan Hendriks <stefanhen83@gmail.com>
2012-01-22 19:35:57 +0100

    Add boundaries for pessimistic and optimistic values

--- src/test/java/com/fundynamic/getitdone/domain/EstimateTest.java
+++ src/test/java/com/fundynamic/getitdone/domain/EstimateTest.java
@@ -22,11 +22,72 @@ public class EstimateTest {
 		Assert.assertEquals(12, pert);
 	}
 	
-	public class Estimate {
+	@Test
+	public void mustCreateEstimateWithAverageValueForMostLikely() {
+		Estimate estimate = Estimate.createWithAverageMostLikely(10, 4);
+		Assert.assertEquals(7, estimate.mostLikely);
+	}
+	
+	@Test
+	public void mustLowerOptimisticByOneForPessimisticWhenPessimisticIsLowerThanOptimistic() {
+		Estimate estimate = new Estimate();
+		estimate.setOptimistic(4);
+		estimate.setPessimistic(3);
+		Assert.assertEquals(2, estimate.optimistic);
+	}
+
+	@Test
+	public void mustUpPessimisticByOneForOptimisticWhenOptimisticIsHigherThanPessimistic() {
+		Estimate estimate = new Estimate();
+		estimate.setPessimistic(4);
+		estimate.setOptimistic(6);
+		Assert.assertEquals(7, estimate.pessimistic);
+	}
+	
+	@Test
+	public void mustNotAllowPessimisticBeLowerThanOne() {
+		Estimate estimate = new Estimate();
+		estimate.setPessimistic(0);
+		Assert.assertEquals(1, estimate.pessimistic);
+	}
+
+	@Test
+	public void mustNotAllowOptimisticBeLowerThanZero() {
+		Estimate estimate = new Estimate();
+		estimate.setOptimistic(-1);
+		Assert.assertEquals(0, estimate.optimistic);
+	}
+	
+	@Test 
+	public void mustReturnEstimateWithSumOfEstimates() {
+		
+	}
+	
+	public static class Estimate {
 
 		private int optimistic = 0;
 		private int pessimistic = 0;
 		private int mostLikely = 0;
+
+		public Estimate() {
+			setOptimistic(0);
+			setPessimistic(1);
+			setMostLikely(1);
+		}
+		
+		public Estimate(int optimistic, int pessimistic, int mostLikely) {
+			setOptimistic(optimistic);
+			setPessimistic(pessimistic);
+			setMostLikely(mostLikely);
+		}
+		
+		public static Estimate createWithAverageMostLikely(int optimistic, int pessimistic) {
+			Estimate estimate = new Estimate();
+			estimate.setPessimistic(pessimistic);
+			estimate.setOptimistic(optimistic);
+			estimate.setMostLikely((pessimistic + optimistic) / 2);
+			return estimate;
+		}
 		
 		// Formula: (O + 4M + P)/6
 		public int getPERTEstimate() {
@@ -36,14 +97,34 @@ public class EstimateTest {
 
 		public void setOptimistic(int optimistic) {
 			this.optimistic = optimistic;
+			if (this.optimistic < 0) setOptimistic(0);
+			if (optimistic > pessimistic) {
+				setPessimistic((optimistic + 1));
+			}
 		}
 
 		public void setPessimistic(int pessimistic) {
 			this.pessimistic = pessimistic;
+			if (this.pessimistic < 1) setPessimistic(1);
+			if (pessimistic < optimistic) {
+				setOptimistic((pessimistic - 1)); 
+			}
 		}
 
 		public void setMostLikely(int mostLikely) {
 			this.mostLikely = mostLikely;
+		}
+
+		public int getOptimistic() {
+			return optimistic;
+		}
+
+		public int getPessimistic() {
+			return pessimistic;
+		}
+
+		public int getMostLikely() {
+			return mostLikely;
 		}
 
 	}
