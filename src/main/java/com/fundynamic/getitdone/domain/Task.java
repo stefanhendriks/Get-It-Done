a5dc274d94ac979d8586389abd0f20364dd2a5fc
Stefan Hendriks <stefanhen83@gmail.com>
2012-01-22 11:57:04 +0100

    Add first test for Task. Assign to worker. Add sub task

--- src/main/java/com/fundynamic/getitdone/domain/Task.java
+++ src/main/java/com/fundynamic/getitdone/domain/Task.java
@@ -1,5 +1,29 @@
 package com.fundynamic.getitdone.domain;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import com.fundynamic.getitdone.TaskTest.Worker;
+
 public class Task {
+
+	private Worker worker;
+	private List<Task> subTasks = new LinkedList<Task>();
+	
+	public void setAssignedWorker(Worker worker) {
+		this.worker = worker;
+	}
+
+	public Worker getAssignedWorker() {
+		return worker;
+	}
+
+	public void addSubTask(Task subTask) {
+		subTasks.add(subTask);
+	}
+
+	public List<Task> getSubTasks() {
+		return subTasks;
+	}
 
 }
